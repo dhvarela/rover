07cffc209e470094e0f40b14620666fca4403b86
Daniel <danielhdezvarela@gmail.com>
2022-02-14 23:52:24 +0100

    refactor RoversCommand.php - allow obstacles as argument - end command with a summary

--- rover/src/UI/Command/RoversCommand.php
+++ rover/src/UI/Command/RoversCommand.php
@@ -5,13 +5,12 @@ namespace App\UI\Command;
 use App\Rover\Planet\Application\PlanetCreator;
 use App\Rover\Planet\Domain\Dimensions;
 use App\Rover\Rover\Application\RoverCreator;
+use App\Rover\Rover\Domain\Instructions;
 use App\Rover\Shared\Domain\Coordinate;
 use App\Rover\Shared\Domain\Direction;
-use App\Rover\Rover\Domain\Instructions;
 use Exception;
 use InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -32,7 +31,12 @@ class RoversCommand extends Command
             ->addArgument('pos-x', InputArgument::REQUIRED, 'Rover coordinate X')
             ->addArgument('pos-y', InputArgument::REQUIRED, 'Rover coordinate Y')
             ->addArgument('direction', InputArgument::REQUIRED, 'Rover direction')
-            ->addArgument('instructions', InputArgument::REQUIRED, 'Rover instructions');
+            ->addArgument('instructions', InputArgument::REQUIRED, 'Rover instructions')
+            ->addArgument(
+                'planet-obstacles',
+                InputArgument::IS_ARRAY | InputArgument::OPTIONAL,
+                'Planet obstacles (separate multiple coordinates with a space) eg. 2,1 5,5'
+            );
     }
 
     public function __construct(PlanetCreator $planetCreator, RoverCreator $roverCreator)
@@ -48,29 +52,40 @@ class RoversCommand extends Command
         $io = new SymfonyStyle($input, $output);
 
         try {
+            $planetObstacles = $this->getObstacles($input->getArgument('planet-obstacles'));
+
             $planet = $this->planetCreator->execute(
-                new Dimensions($input->getArgument('planet-dimensions'))
+                new Dimensions($input->getArgument('planet-dimensions')),
+                ...$planetObstacles
             );
 
-            $coordinate = new Coordinate(
-                $input->getArgument('pos-x'),
-                $input->getArgument('pos-y')
+            $rover = $this->roverCreator->execute(
+                $planet,
+                new Coordinate(
+                    $input->getArgument('pos-x'),
+                    $input->getArgument('pos-y')
+                ),
+                new Direction($input->getArgument('direction')),
+                new Instructions($input->getArgument('instructions'))
             );
 
-            $direction    = new Direction($input->getArgument('direction'));
-            $instructions = new Instructions($input->getArgument('instructions'));
-
-            $rover = $this->roverCreator->execute($planet, $coordinate, $direction, $instructions);
-
-            $rover->executeInstructions();
+            $obstacle = $rover->executeInstructions();
 
             $io->definitionList(
-                'Movement History :', //TODO movement history
-                new TableSeparator(),
                 'Final rover position:',
                 ['x' => $rover->getCoordinate()->getX()],
-                ['y' => $rover->getCoordinate()->getY()]
+                ['y' => $rover->getCoordinate()->getY()],
+                ['Direction' => $rover->getDirection()->value()]
             );
+            if ($obstacle) {
+                $io->warning(
+                    sprintf(
+                        'Obstacle found at: %d, %d',
+                        $obstacle->getCoordinate()->getX(),
+                        $obstacle->getCoordinate()->getY()
+                    )
+                );
+            }
             $io->success("Rover expedition completed");
 
         } catch (InvalidArgumentException $e) {
@@ -81,5 +96,16 @@ class RoversCommand extends Command
         }
 
         return Command::SUCCESS;
+    }
+
+    private function getObstacles($planetObstacles): array
+    {
+        $obstacles = [];
+        foreach ($planetObstacles as $obstacle) {
+            $coordinates = explode(',', $obstacle);
+            $obstacles[] = new Coordinate($coordinates[0], $coordinates[1]);
+        }
+
+        return $obstacles;
     }
 }
